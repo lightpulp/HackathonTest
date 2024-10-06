f5290f5e465690657a24667e16d6ff439c7eb7e4
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 09:06:35 +0000

    Update Home.tsx, LogIn.tsx

--- src/frontend/src/Home.tsx
+++ src/frontend/src/Home.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./Styles/App.scss";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   //const { loggedIn, email } = props
