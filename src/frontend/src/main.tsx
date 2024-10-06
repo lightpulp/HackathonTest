a96029f094dd92233514ba11bbb01d61c12a57b3
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 08:56:52 +0000

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -9,6 +9,6 @@ import Login from './LogIn';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <LogIn/>
+    <App/>
   </React.StrictMode>,
 );
