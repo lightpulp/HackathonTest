49b12da32285a0381279ea3dc0d0799e34bb211b
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 07:54:49 +0000

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import LogIn from './LogIn';
 import './index.scss';
+import './Styles/App.scss';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
