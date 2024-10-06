3bc9c46be63d970f5da6bde5fe7682d3def4bdea
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 07:56:05 +0000

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import LogIn from './LogIn';
+import Home from './Home';
 import './index.scss';
 import './Styles/App.scss';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <LogIn />
+    <Home />
   </React.StrictMode>,
 );
