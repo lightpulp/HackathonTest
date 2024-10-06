83310478d4febeab1274a9151b125610728bc137
Sempiiee <163949158+Sempiiee@users.noreply.github.com>
2024-10-06 14:12:35 +0800

    Update LogIn.tsx

--- src/frontend/src/LogIn.tsx
+++ src/frontend/src/LogIn.tsx
@@ -2,20 +2,23 @@ import { useState } from 'react';
 
 function LogIn_Button() {
   return (
-    <button>
-      
-    </button>
+
   );
 }
 
-export default function LogIn() {
+export default function LogIn(event: any) {
   return (
     <div>
-      <h1>Water FootPrint</h1>
+      
+      <form action="#" onSubmit={LogIn}>
+        <label htmlFor="userName">Enter your Username: &nbsp;</label>
+        <input id="userName" alt="name" type="text" />
+        <label htmlFor="userPassword">Enter your Password: &nbsp;</label>
+        <input id="name" alt="password" type="password" />
+        <button type="submit">LogIn</button>
+      </form>
       
     </div>
-
-    <LogIn_Buton />
   );
 }
 
