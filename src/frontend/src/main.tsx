b6e2a105e5e0277e56ff8c0e1346e6020fbe7181
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 14:27:04 +0000

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import LogIn from './LogIn';
 import Home from './Home';
+import RouterComponent from "./RouterComponent";
 import './index.scss';
 import './Styles/App.scss';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <Home email={""} loggedIn={false} setLoggedIn={function (value: React.SetStateAction<boolean>): void {
+      throw new Error("Function not implemented.");
+    } } />
   </React.StrictMode>,
 );
