bc240c2473fd984fd8c634430e4a9f9a2895a544
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 09:13:54 +0000

    Update LogIn.tsx

--- src/frontend/src/LogIn.tsx
+++ src/frontend/src/LogIn.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./index.scss";
 
-const LogIn = () => {
+const LogIn: React.FC = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [emailError, setEmailError] = useState('')
