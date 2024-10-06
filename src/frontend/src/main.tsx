89452847fdd5e5b2c7907e9875358848664b97d5
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 09:21:33 +0000

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -8,6 +8,6 @@ import './Styles/App.scss';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <LogIn />
   </React.StrictMode>,
 );
