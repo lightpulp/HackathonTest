5047428e13c91456d1579973ffede5be654e9e0a
Sempiiee <163949158+Sempiiee@users.noreply.github.com>
2024-10-06 14:06:18 +0800

    Update LogIn.tsx

--- src/frontend/src/LogIn.tsx
+++ src/frontend/src/LogIn.tsx
@@ -1,1 +1,23 @@
 import { useState } from 'react';
+
+function LogIn_Button() {
+  return (
+    <button>
+      
+    </button>
+  );
+}
+
+export default function LogIn() {
+  return (
+    <div>
+      <h1>Water FootPrint</h1>
+      
+    </div>
+
+    <LogIn_Buton />
+  );
+}
+
+
+export default LogIn;
