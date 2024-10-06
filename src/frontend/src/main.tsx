537924de9eb803bde94b59f3be77912257c9da51
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-06 07:15:24 +0000

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import LogIn from './Login';
+import LogIn from './LogIn';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
