6471313045a8c3e85d64509115655fd0da0e4b65
Sempiiee Sempie <iguico.223994@sanfernando.sti.edu.ph>
2024-10-16 03:42:00 +0000

    Update LogIn.tsx

--- src/frontend/src/LogIn.tsx
+++ src/frontend/src/LogIn.tsx
@@ -52,7 +52,7 @@ const LogIn: React.FC<LoginProps> = ({ setLoggedIn, setEmail }) => {
       navigate('/App');
       console.log('Form is valid, proceed with authentication...');
     }
-    navigate('/SignUp');
+    navigate('/App');
   };
 
   const handleTabClick = (tab: string) => {
